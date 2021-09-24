8294cd9cdb133a2f5ca02769724b6c5adf85b477
Pruthvi Shrikaanth <38621789+Blue0Tech@users.noreply.github.com>
2021-09-24 17:40:50 +0100

    Food app

---
+++ createButton.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent('create-button',{
+    init: function() {
+        var button1 = document.createElement('button');
+        button1.innerHTML = 'Rate Us';
+        button1.setAttribute('id','ratingButton');
+        button1.setAttribute('class','btn-warning');
+        var button2 = document.createElement('button');
+        button2.innerHTML = 'Order Now';
+        button2.setAttribute('id','orderButton');
+        button2.setAttribute('class','btn-warning');
+        var buttonDiv = document.getElementById('button-div');
+        buttonDiv.appendChild(button1);
+        buttonDiv.appendChild(button2);
+    }
+});
